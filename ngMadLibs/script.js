d89e29e47c48a23ee4e0e3f0cbbbfc5e314f1230
sasrsc <richard.clowes@sas.com>
2015-04-03 06:58:29 -0400

    new branch

--- ngMadLibs/script.js
+++ ngMadLibs/script.js
@@ -1,10 +1,21 @@
 // Code goes here
 
-angular.module('myApp', []).
-controller('myCtrl', function($scope) {
+angular.module('myApp', [])
+
+.controller('myCtrl', function($scope,$log) {
    
-   $scope.submit=function() {
-   		
-   };
+   	$scope.formData=[];
+   	$scope.formData.femaleName="Valerie";
+   	$scope.formData.tediousTask="Brushing Teeth";
+   	$scope.formData.dirtyTask="Cleaning Toilets";
+   	$scope.formData.celebrity="Freddie Mercury";
+   	$scope.formData.uselessSkill="Speaking Latin";
+   	$scope.formData.obnoxiousCelebrity="Too many to name";
+
+
+	$scope.submit=function() {
+
+		$log.info("OK, let's play");
+	};
  
 });
