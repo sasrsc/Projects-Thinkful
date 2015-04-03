5dd7f52a2f0143d96a205b469efabec0deebebb9
sasrsc <richard.clowes@sas.com>
2015-04-03 07:43:36 -0400

    adjusted his/her stuff

--- ngMadLibs/script.js
+++ ngMadLibs/script.js
@@ -5,13 +5,29 @@ angular.module('myApp', [])
 .controller('myCtrl', function($scope,$log) {
    
    	$scope.formData=[];
-   	$scope.formData.femaleName="Valerie";
+
+
+   	$scope.formData.gender="Male";
+   	$scope.formData.name="Richard";
+   	$scope.formData.hisher="his";
    	$scope.formData.tediousTask="Brushing Teeth";
    	$scope.formData.dirtyTask="Cleaning Toilets";
    	$scope.formData.celebrity="Freddie Mercury";
    	$scope.formData.uselessSkill="Speaking Latin";
    	$scope.formData.obnoxiousCelebrity="Too many to name";
 
+   	$scope.genderAdjust=function() {
+
+   		$log.info("Gender is " + $scope.formData.gender);
+   	  	
+   	  	if ($scope.formData.gender=="Male") {
+   	   		$scope.formData.name="Richard";
+   	   		$scope.formData.hisher="his";
+   		} else {
+   	   		$scope.formData.name="Valerie";
+   	   		$scope.formData.hisher="her";
+   		}
+   	};
 
 	$scope.submit=function() {
 
